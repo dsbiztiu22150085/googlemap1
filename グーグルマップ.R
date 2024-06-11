7eb721cdcc578c757bb3e418c54ef590e1b158ef
dsbiztiu22150085 <m.yuinanana@gmail.com>
2024-06-11 17:56:41 +0900

    add

---
+++ グーグルマップ.R
@@ -0,0 +1,14 @@
+d <- read.csv('campus_location_TIU.csv')
+
+library(DT)
+datatable(d)
+#install.packages('leaflet')
+library(leaflet)
+
+leaflet() %>%
+  addTiles() %>% 
+  addMarkers(lng = d$lng, lat = d$lat,
+             popup = paste0(d$name, '<br>', d$address)) %>% 
+  addMiniMap(positio = 'topright') %>%
+  addScaleBar(position="bottomleft")
+
